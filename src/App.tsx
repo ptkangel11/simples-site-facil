b36a492920c0342e54f050f726978c8e8776bf20
ptkangel11 <88540543+ptkangel11@users.noreply.github.com>
2025-06-03 11:08:01 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+       <BrowserRouter basename="/simples-site-facil">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/confirmar-presenca" element={<ConfirmarPresenca />} />
